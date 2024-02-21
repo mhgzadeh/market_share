569e3eea922721032c1c713d750ae2438186a42c
Mohammad <m.hgzadeh@gmail.com>
2024-02-21 20:57:29 +0100

    init market-share

---
+++ main.py
@@ -0,0 +1,57 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Tuple
+
+from shapely import unary_union
+from shapely.geometry import Polygon, Point
+from utils import reproject, de_utm_selector
+
+app = FastAPI()
+
+
+class PolygonBoundary(BaseModel):
+    type: str
+    coordinates: List[List[Tuple[float, float]]]
+
+
+class Feature(BaseModel):
+    type: str
+    geometry: PolygonBoundary
+
+
+class MarketShareRequest(BaseModel):
+    city_boundary: Feature
+    points: List[Tuple[float, float]]
+
+
+@app.post("/market-share/")
+async def calculate_market_share(request: MarketShareRequest):
+    # Re-projecting the city boundary to utm
+    city_boundary_data = request.city_boundary.geometry.coordinates
+    city_boundary = Polygon(city_boundary_data[0])
+    epsg_city = de_utm_selector(city_boundary_data)
+    city_boundary_reprojected = reproject(city_boundary, epsg_city)
+
+    points_data = request.points
+    buffer_radius = 500
+    buffer_geoms = []
+    for point in points_data:
+        # Re-projecting the points to utm
+        epsg_point = de_utm_selector(point)
+        point_geom = Point(point)
+        point_geom_reprojected = reproject(point_geom, epsg_point)
+
+        # Check whether the points are inside the boundary or not
+        if city_boundary_reprojected.intersects(point_geom_reprojected):
+            buffer_geom = point_geom_reprojected.buffer(buffer_radius)
+            buffer_geoms.append(buffer_geom)
+
+    # Dissolve all the buffer geometries into a single geometry
+    dissolved_buffer = unary_union(buffer_geoms)
+
+    total_buffer_area = dissolved_buffer.area
+    total_city_area = city_boundary_reprojected.area
+
+    market_share = (total_buffer_area / total_city_area) * 100
+
+    return {"market_share": round(market_share, 2)}
---
+++ utils.py
@@ -0,0 +1,20 @@
+import pyproj
+from shapely.ops import transform
+
+
+def de_utm_selector(coordinates):
+    if isinstance(coordinates, list):
+        min_lon = min(coord[0] for coord in coordinates)[0]
+    else:
+        min_lon = coordinates[0]
+
+    return '25832' if 6 <= min_lon < 12 else '25833'
+
+
+def reproject(shapely_feature, utm_epsg):
+    project = pyproj.Transformer.from_crs(
+        "EPSG:4326", f"EPSG:{utm_epsg}", always_xy=True
+    ).transform
+    transformed_feature = transform(project, shapely_feature)
+
+    return transformed_feature
